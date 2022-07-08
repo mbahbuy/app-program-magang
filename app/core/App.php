a3ceced749163ed06c1638cc5694322b13dc955b
mbahbuy <mbahbagonku@gmail.com>
2022-07-09 02:37:32 +0700

    p

--- app/core/App.php
+++ app/core/App.php
@@ -5,12 +5,15 @@ class App {
     protected $method = 'index';
     protected $params = [];
 
-    public function __construct(){
+    public function __construct()
+    {
         $url = $this->parseURL();
         
         //controller
-        if( isset( $url[0] ) ){
-            if( file_exists( 'app/controllers/' . $url[0] . '.php' ) ){
+        if( isset( $url[0] ) )
+        {
+            if( file_exists( 'app/controllers/' . $url[0] . '.php' ) )
+            {
                 $this->controller = $url[0];
                 unset($url[0]);
             }            
@@ -20,15 +23,18 @@ class App {
         $this->controller = new $this->controller;
 
         // method
-        if( isset( $url[1] ) ){
-            if( method_exists( $this->controller, $url[1] ) ){
+        if( isset( $url[1] ) )
+        {
+            if( method_exists( $this->controller, $url[1] ) )
+            {
                 $this->method = $url[1];
                 unset( $url[1] );
             }
         }
 
         // params
-        if( !empty( $url ) ){
+        if( !empty( $url ) )
+        {
             $this->params = array_values($url);
         }
 
@@ -37,8 +43,10 @@ class App {
 
     }
 
-    public function parseURL(){
-        if( isset($_GET['url']) ){
+    public function parseURL()
+    {
+        if( isset($_GET['url']) )
+        {
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
